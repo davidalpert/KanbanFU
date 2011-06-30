1ab1495a9c3a50058e6ed5f7c98390bf38d8586c
Marc Jeanson <marc@redlinesoftware.com>
2011-06-29 23:35:04 -0500

    setting up the default testing stack

--- Gemfile
+++ Gemfile
@@ -2,9 +2,6 @@ source 'http://rubygems.org'
 
 gem 'rails', '3.1.0.rc4'
 
-# Bundle edge Rails instead:
-# gem 'rails',     :git => 'git://github.com/rails/rails.git'
-
 gem 'sqlite3'
 
 # Asset template engines
@@ -12,14 +9,21 @@ gem 'sass-rails', "~> 3.1.0.rc"
 gem 'coffee-script'
 gem 'uglifier'
 
-
-
-# Use unicorn as the web server
-# gem 'unicorn'
-
 # Deploy with Capistrano
-# gem 'capistrano'
+gem 'capistrano'
 
 # To use debugger
 # gem 'ruby-debug19', :require => 'ruby-debug'
+
+group :development, :test do
+  gem 'rspec-rails'
+  gem 'cucumber-rails'
+  gem 'capybara'
+  gem 'shoulda'
+  gem 'fabrication'
+  gem 'launchy'
+  gem 'faker'
+  gem 'spork', '~> 0.9.0.rc'
+  gem 'database_cleaner'
+end
 
---
+++ spec/spec_helper.rb
@@ -0,0 +1,29 @@
+require 'rubygems'
+require 'spork'
+
+ENV["RAILS_ENV"] ||= 'test'
+
+Spork.prefork do
+  # Loading more in this block will cause your tests to run faster. However,
+  # if you change any configuration or code from libraries loaded here, you'll
+  # need to restart spork for it take effect.
+
+  require File.expand_path("../../config/environment", __FILE__)
+  require 'rspec/rails'
+  require 'capybara/rspec'
+  require 'capybara/rails'
+
+  # Requires supporting ruby files with custom matchers and macros, etc,
+  # in spec/support/ and its subdirectories.
+  Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+
+  RSpec.configure do |config|
+    config.mock_with :rspec
+    config.use_transactional_fixtures = true
+  end
+end
+
+Spork.each_run do
+  # This code will be run each time you run your specs.
+
+end
