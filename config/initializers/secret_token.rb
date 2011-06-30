d5fe0c8cce71b328246ee28f2c46c4491c0967c2
Marc Jeanson <marc@redlinesoftware.com>
2011-06-29 23:13:45 -0500

    stock rails app without jquery or test::unit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kanbanfu::Application.config.secret_token = '9336aefcac57622f6834afeb7c405b8e1f695147100a0b69f9795e4c8c63cd1459566e23b484bca6956d77d00be8538c31ac63bd32d91efd322af5b99606fe7b'
