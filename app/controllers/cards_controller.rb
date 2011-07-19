395fa4ed6bdd2f99b1c0cc84efe02ded3203d3ba
Amir Barylko <amir@barylko.com>
2011-07-19 18:17:43 -0500

    Refactored the controller

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -3,13 +3,12 @@ class CardsController < ApplicationController
   
   def index
     project = Project.find(params[:project_id])
+    
     respond_to do |format|
       format.json do
-        if project
-          render :json => {cards: project.cards}, :except => [:project_id, :created_at, :updated_at]
-        else
-          render :json => @project.to_json, :status => 404
-        end
+        item = {cards: project.cards} if project
+        status = project ? :ok : 404
+        render :json => item, :status => status, :except => [:project_id, :created_at, :updated_at]
       end
     end
   end
