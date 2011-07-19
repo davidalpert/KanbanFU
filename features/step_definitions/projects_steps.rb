370e19bc2a13b7e09e7f9eeed323c79ece68fdf5
Amir Barylko <amir@barylko.com>
2011-07-19 16:37:59 -0500

    Add to_json

--- features/step_definitions/projects_steps.rb
+++ features/step_definitions/projects_steps.rb
@@ -4,7 +4,7 @@ end
 
 Then /^the response should be a collection of "(.+)" with:$/ do |root, table|
   table.hashes.each do |row|
-    json = row.to_s.gsub("=>", ":")
+    json = row.to_json
     path = "#{root}/#{row['id'].to_i - 1}"
     Then(%Q{the JSON at "#{path}" should be #{json}})
   end
