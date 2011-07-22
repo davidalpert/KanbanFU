4760c9f4b7380cd27bec73910283b124923a21a6
Amir Barylko <amir@barylko.com>
2011-07-21 22:01:51 -0500

    Added context and it should... expressions

--- spec/controllers/cards_controller_spec.rb
+++ spec/controllers/cards_controller_spec.rb
@@ -6,21 +6,49 @@ describe CardsController do
     @project = stub_model(Project, id: 1, name: "Blazing Saddles", description: "Movie")
     @cards = (1..1).collect {|i| stub_model(Card, :project_id => @project.id, :id => i, :title => "card_#{i}", :description => "description_#{i}") }
     @project.stub(:cards).and_return(@cards)
+    @json = {cards: @cards}.to_json(:except => [:project_id, :created_at, :updated_at])
   end
 
-  describe "GET on 'index' via JSON" do
-    it "returns the collection of cards" do
-      Project.stub(:find).with(@project.id).and_return(@project)
-      get :index, :format => :json, :project_id => @project.id
-      json = {cards: @cards}.to_json(:except => [:project_id, :created_at, :updated_at])
-      response.body.should eq(json)
+  describe ".index with JSON format" do
+    context "resource found" do
+      before do
+        Project.stub(:find).with(@project.id).and_return(@project)
+        get :index, :format => :json, :project_id => @project.id
+      end
+      
+      it { response.body.should eq(@json) }
     end
 
-    it "returns an error when the project does not exist" do
-      Project.stub(:find).with(@project.id).and_return(nil)
-      get :index, :format => :json, :project_id => @project.id
-      should respond_with 404
+    context "resource not found" do
+      before do
+        Project.stub(:find).with(@project.id).and_return(nil)
+        get :index, :format => :json, :project_id => @project.id
+      end
+      
+      it { should respond_with :missing }
     end
+  end
+
+  describe ".show with JSON format" do
+    before do
+      card = @project.cards.first
+      Project.stub(:find).with(@project.id).and_return(@project)
+      Card.stub(:find).with(card.id).and_return(card)
+      get :index, :format => :json, :project_id => @project.id, :id => card.id
+    end
+    
+    context "when the resource exists" do
+      it { should respond_with :success }
+    end
+    
+    context "when the resource does not exists"  do
+      it { should respond_with :missing }
+    end
+    
+    context "when the project does not exist" do 
+      it { should respond_with :missing }
+    end
+
   end
 
 end
--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -3,15 +3,18 @@ require 'spec_helper'
 describe ProjectsController do
 
   before(:each) do
-    @projects = (1..10).collect {|i| stub_model(Project, :id => i, :name => "project_#{i}", :description => "description_#{i}") }
+    @projects = (1..1).collect {|i| stub_model(Project, :id => i, :name => "project_#{i}", :description => "description_#{i}") }
   end
 
-  describe 'GET on index via JSON' do
-    it 'returns the collection of projects' do
+  describe '.index format JSON' do
+    before do 
       Project.stub(:all).and_return(@projects)
       get :index, :format => :json
-      response.body.should eq({projects: @projects}.to_json)
     end
+
+    it { should respond_with(:success) }
+    it { should respond_with_content_type(:json) }
+    it { response.body.should eq({projects: @projects}.to_json) }
   end
 
 end
