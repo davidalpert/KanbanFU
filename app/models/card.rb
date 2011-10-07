45114f979a747b21faadfe70301c1d20beff4bf7
Marc Jeanson <marc@redlinesoftware.com>
2011-10-07 17:52:48 -0500

    Adding has_many cards to phases. Also using new hash syntax.

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,13 +1,13 @@
 class Card < ActiveRecord::Base
   belongs_to :phase
-  validates :title, :presence => true
-  validates :phase, :presence => true
-  after_initialize :init  
-  
+  validates :title, presence: true
+  validates :phase, presence: true
+  after_initialize :init
+
   private
     def init
       self.blocked_time ||= 0
       self.waiting_time ||= 0
     end
-  
+
 end
--- spec/models/phase_spec.rb
+++ spec/models/phase_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe Phase do
+  it { should have_many(:cards).dependent(:destroy) }
   it { should have_db_column(:name).of_type(:string)  }
   it { should have_db_column(:description).of_type(:string) }
 end
