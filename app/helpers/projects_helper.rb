a887f9e6889cd423ff2424c215407aa9f0cc36ac
Marc Jeanson <marc@redlinesoftware.com>
2011-10-08 02:22:37 -0500

    SPIKE: Adding the card description and size to the card UI

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -3,4 +3,8 @@ module ProjectsHelper
     width = 100.0 / phase_count
     {style: sprintf("width: %5.2f%", width)}
   end
+
+  def card_size(card)
+    card.size.blank? ? '0' : card.size
+  end
 end
